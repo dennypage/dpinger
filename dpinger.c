bf18a6e2a8b8f89ad595efdbe89099fc61c6ddf3
Denny Page <dennypage@me.com>
2020-06-07 13:23:33 -0700

    Update copyright

--- dpinger.c
+++ dpinger.c
@@ -1,6 +1,6 @@
 
 //
-// Copyright (c) 2015-2017, Denny Page
+// Copyright (c) 2015-2020, Denny Page
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
